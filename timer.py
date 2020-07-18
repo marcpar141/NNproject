0894908406280bfd19fc40c7aee77f13312d5d35
Artur Oleksiński <artur.oleksinski99@gmail.com>
2020-07-18 17:07:31 +0200

    Update timer.py

--- timer.py
+++ timer.py
@@ -6,9 +6,10 @@ In this example (actually made for timing this methods)
 we are using path to binary data db/nose.bin.
 Also type of data we want to withdraw, image as matix
 You can use this to test between Python versions and Pypy.
-Between Python 3.8 and Pypy was difference of about 4 sec! to load and ptint data
+Between Python 3.8 and Pypy was difference of about 0.7sec! to load and ptint data
 """
 start_time = time.time()
 get = get_db.get_db("db/nose.bin", "image")
 
 print("%s" % (time.time() - start_time))
+print(get.data)
