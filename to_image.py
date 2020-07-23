9a52bea52b150a30eddf630108616b9697a7d0f1
ArturOle <artur.oleksinski99@gmail.com>
2020-07-23 23:31:56 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	timer.py

--- to_image.py
+++ to_image.py
@@ -1,43 +1,55 @@
 import get_bin
 import numpy as np
-import matplotlib.pyplot as plt
+import turtle
 
 
 class to_image:
-    def __init__(self):
-        self.data = np.array(get_bin.get_bin("D://db/car.bin").data["image"])
+    def __init__(self, class_name):
+        self.class_name = class_name
+        self.data = np.array(get_bin.get_bin("D://db/{}.bin".format(self.class_name)).data["image"])
         self.my_array = []
         self._fill()
 
     def _fill(self):
-        for i in self.data:
-            lst = list(i)
+        i = 0
+        while i < len(self.data):
+            strokes = []
+            lst = list(self.data[i])
             for x in range(len(lst[0])):
-                self.my_array.append([lst[0][x], lst[1][x]])
+                strokes.append([lst[0][x], lst[1][x]])
+
+            self.my_array.append(strokes)
+            i += 1
 
     def _draw(self):
-        for i in range(len(self.my_array) + 1):
-            if i == len(self.my_array):
-                break
-            V1 = self.my_array[i - 1]
-            V2 = self.my_array[i]
+        s = turtle.Screen()
+        t = turtle.Turtle()
 
-            plt.quiver(V1[0], V1[1] * (-1), V2[0], V2[1] * (-1))
+        for j in self.my_array:
+            V_prev = [-1, -1]
+            print(j)
+            for i, x in enumerate(j):
+                x = list(x)
+                if i - 1 == len(j):
+                    break
+
+                V = x
+                V[1] = V[1] * (-1)
+
+                if i == 0:
+                    t.penup()
+                    t.goto(V)
+                else:
+                    t.pendown()
+                    t.goto(V)
+
+                V_prev = V
 
     def show(self):
         self._draw()
-        plt.show()
-
-    def save(self):
-        self._draw()
-        plt.imsave("img.png")
+        turtle.done()
 
 
-# TEST
-"""
 if __name__ == "__main__":
-    img = to_image()
+    img = to_image("car")
     img.show()
-    img.show()
-    img.save()
-"""
