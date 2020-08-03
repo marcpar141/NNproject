700e6466f7efa613bf4bd132cf8c057402c9a2bf
ArturOle <artur.oleksinski99@gmail.com>
2020-08-03 22:28:38 +0200

    Merge remote-tracking branch 'origin/master'

--- to_image.py
+++ to_image.py
@@ -90,19 +90,32 @@ class to_image:
 
 
 class all_to_image:
-    def __init__(self, class_name):
+    def __init__(self, class_name, how_many=10):
+        self.how_many = how_many
         self.class_name = class_name
         self.my_array = []
         self.index = 0
-        self._to_ps()
+        self._ps_everything()
         self._chop_all_blank()
+
+    def _fill(self):  # Zrób ograniczenie oraz żeby żółw rysował
+        i = 0
+        while i < len(self.data):
+            strokes = []
+            lst = list(self.data[i])
+            for x in range(len(lst[0])):
+                strokes.append([lst[0][x], lst[1][x]])
+
+            self.my_array.append(strokes)
+            i += 1
+            print(strokes)
 
     def _draw(self):
         self.s = turtle.Screen()
         self.s.colormode(1)
-        t = turtle.Turtle()
-        t.speed('fastest')
-        t.hideturtle()
+        self.t = turtle.Turtle()
+        self.t.speed('fastest')
+        self.t.hideturtle()
 
         for j in self.my_array:
             V_prev = [-1, -1]
@@ -116,25 +129,34 @@ class all_to_image:
                 V = x
                 V[1] = V[1] * (-1)
                 if i == 0:
-                    t.penup()
-                    t.goto(V)
+                    self.t.penup()
+                    self.t.goto(V)
                 else:
-                    t.pendown()
-                    t.goto(V)
+                    self.t.pendown()
+                    self.t.goto(V)
 
                 V_prev = V
 
+
     def _to_ps(self):
+        self._fill()
         self._draw()
+        self.my_array = []
         self.s = self.s.getcanvas().postscript(file="{}{}.ps".format(self.class_name, self.index))
-        shutil.move("D://Repos/NNproject/{}.ps".format(self.class_name), "D://Repos/NNproject/ps_folder")
-        os.remove("D://Repos/NNproject/{}{}.ps".format(self.class_name, self.index))
+        self.t.clear()
+        shutil.move("D://Repos/NNproject/{}{}.ps".format(self.class_name, self.index), "D://Repos/NNproject/ps_folder")
 
     def _ps_everything(self):
         self.data = get_bin.get_bin("D://db/{}.bin".format(self.class_name)).return_all()
-        for drawing in self.data:
-            self._to_ps()
-            self.index += 1
+        self.clear_ps()
+        for i, drawing in enumerate(self.data):
+            if i < self.how_many:
+                print(drawing)
+                self.data = drawing
+                self._to_ps()
+                self.index += 1
+            else:
+                break
 
     def _chop_all_blank(self):
         self._conv_all_ps_png()
@@ -156,11 +178,21 @@ class all_to_image:
 
     @staticmethod
     def _conv_all_ps_png():
-        list_ps = glob.glob('*.ps')
+        list_ps = glob.glob('ps_folder/*.ps')
         for file in list_ps:
             root = file[:-3]
             pngfile = ''.join([root, ".png"])
             os.system(''.join(['magick', ' ', 'convert ', file, " ", pngfile]))
+        png_in_ps = glob.glob('ps_folder/*.png')
+        for file in png_in_ps:
+            shutil.move("D://Repos/NNproject/{}".format(file),
+                        "D://Repos/NNproject/png_folder")
+
+    @staticmethod
+    def clear_ps():
+        for filename in os.listdir("D://Repos/NNproject/ps_folder/"):
+            filepath = os.path.join("D://Repos/NNproject/ps_folder/", filename)
+            os.unlink(filepath)
 
 
 if __name__ == "__main__":
